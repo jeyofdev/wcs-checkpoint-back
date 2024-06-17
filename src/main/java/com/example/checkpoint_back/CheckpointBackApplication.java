1a72bb1f13284076da71e21048093066de5b7f80
jeyofdev <jgregoire.dev@gmail.com>
2024-06-17 09:23:26 +0200

    init project with spring

---
+++ src/main/java/com/example/checkpoint_back/CheckpointBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.checkpoint_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckpointBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckpointBackApplication.class, args);
+	}
+
+}
